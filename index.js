0e41ca5a90a559e60b03037f7e2c8780e30f29ec
Antonio Fernandez Paternina <info@antoniofernandez.pro>
2020-06-05 20:10:34 -0500

    errors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const Sentry = require('@sentry/node');
 
 const api = require('./api');
 const config = require('./config');
 
+Sentry.init({ dsn: 'https://c0f0fb4d7c914ae69ee83444d0ccb4d9@o403578.ingest.sentry.io/5266428' });
 const app = express();
 app.use(express.json());
 app.use('/api', api);
